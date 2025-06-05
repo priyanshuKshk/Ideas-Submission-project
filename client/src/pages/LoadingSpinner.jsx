aba1ea8ed8cd7d6283c2150e7dc7c7e085bf5aff
priyanshuKshk <kshkpriyanshu64@gmail.com>
2025-06-06 00:03:57 +0530

    loading..

--- client/src/pages/LoadingSpinner.jsx
+++ client/src/pages/LoadingSpinner.jsx
@@ -6,6 +6,13 @@ export default function LoadingSpinner() {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-40 flex flex-col items-center justify-center z-50">
       <div className="w-28"
+        style={{
+          width: '200px',
+          height: '200px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
       >
         <Lottie animationData={animationData} loop={true} />
       </div>
